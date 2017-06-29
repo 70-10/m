9bda17f68073f96a7de9e19ccc37667676b82f40
70_10 <7010.dev@gmail.com>
2017-06-29 13:32:25 +0900

    [mod] separate config command

--- bin/m.js
+++ bin/m.js
@@ -2,16 +2,24 @@
 
 const pkg = require("../package.json");
 const prog = require("commander");
+const debug = require("debug")("m:main");
 
 const actions = require("../lib/actions");
+const commands = require("../lib/commands");
 
 if (process.argv.length < 3) {
   process.argv.push("--help");
 }
 
+// worst case....
+process.on("unhandledRejection", (err, p) => {
+  debug(err);
+  debug(p);
+});
+
 prog.version(pkg.version);
 
-prog.command("config").alias("c").action(actions.config);
+prog.command("config").alias("c").action(commands.config);
 prog.command("view").alias("v").action(actions.view);
 prog.command("edit").alias("e").action(actions.edit);
 prog.command("list").alias("l").action(actions.list);
