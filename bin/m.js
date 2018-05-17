d1911d8b5ee82ed53f7195fdf6995a22151f0198
70_10 <7010.dev@gmail.com>
2018-05-17 23:51:27 +0900

    [add] prettier

--- bin/m.js
+++ bin/m.js
@@ -19,9 +19,18 @@ process.on("unhandledRejection", (err, p) => {
 
 prog.version(pkg.version);
 
-prog.command("config").alias("c").action(commands.config);
-prog.command("view").alias("v").action(commands.view);
-prog.command("edit").alias("e").action(commands.edit);
+prog
+  .command("config")
+  .alias("c")
+  .action(commands.config);
+prog
+  .command("view")
+  .alias("v")
+  .action(commands.view);
+prog
+  .command("edit")
+  .alias("e")
+  .action(commands.edit);
 
 prog.arguments("<text>").action(actions);
 prog.parse(process.argv);
